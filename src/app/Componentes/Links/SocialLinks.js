252c0794452e494ae5237867a4b21508cfc7da2c
fernandoaneto2 <fernando.neto02@gmail.com>
2024-11-05 04:37:55 -0300

    Topo finzalizado

--- src/app/Componentes/Links/SocialLinks.js
+++ src/app/Componentes/Links/SocialLinks.js
@@ -1,23 +1,25 @@
 
+
 import { FaLinkedin, FaGithub, FaWhatsapp, FaInstagram } from 'react-icons/fa';
 
 const SocialLinks = () => {
   return (
 <div style={{display: 'flex', gap: '25px', alignItems: 'center',}}>
       <a href="https://www.linkedin.com/in/fernando-amorim-33860624a/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn" style={{transition: 'transform 0.3s ease, color 0.3s ease', color: 'inherit' }}>
-        <FaLinkedin size={35} color="#0e76a8" />
+        <FaLinkedin size={35} color="white" />
       </a>
       <a href="https://github.com/fernandoaneto2" target="_blank" rel="noopener noreferrer" aria-label="GitHub" style={{ transition: 'transform 0.3s ease, color 0.3s ease', color: 'inherit'}}>
-        <FaGithub size={35} color="#171515" />
+        <FaGithub size={35} color="white" />
       </a>
       <a href="https://wa.me/+5583998661293" target="_blank" rel="noopener noreferrer" aria-label="WhatsApp" style={{ transition: 'transform 0.3s ease, color 0.3s ease', color: 'inherit'}}>
-        <FaWhatsapp size={35} color="#25D366" />
+        <FaWhatsapp size={35} color="white" />
       </a>
       <a href="https://www.instagram.com/fernandoaneto/" target="_blank" rel="noopener noreferrer" aria-label="Instagram" style={{transition: 'transform 0.3s ease, color 0.3s ease', color: 'inherit'}}>
-        <FaInstagram size={35} color="#E1306C"/>
+        <FaInstagram size={35} color="white"/>
       </a>
     </div>
   );
+
 
 };
 
--- src/app/Componentes/Topo/topo.jsx
+++ src/app/Componentes/Topo/topo.jsx
@@ -2,20 +2,39 @@ import styles from "@/app/Componentes/Topo/topo.module.css"
 import Image from "next/image"
 import fotodeperfil from "@/app/Imagens/fotoDePerfil.png"
 import SocialLinks from "../Links/SocialLinks";
+import { Button, buttonVariants } from "@/components/ui/button"
+import Link from "next/link";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "@/components/ui/tooltip"
+  
 
 export default function Topo(){
     return(
-        <main className={styles.main}>
+        <main className={styles.container}>
             
-            <div style={{width: "100%", height: "100vh", boxSizing: "border-box" , display: "flex", flexDirection: "row", justifyContent: "space-evenly", alignItems: "center", flexWrap: "wrap", padding: "30px"}}>
+            <div style={{width: "100%", height: "100vh", boxSizing: "border-box" , display: "flex", flexDirection: "row", justifyContent: "space-evenly", alignItems: "center", flexWrap: "wrap", padding: "20px"}}>
                 <div className={styles.fotoDePerfil}>
                     <Image src={fotodeperfil} width={400} height={400} style={{diplay: "flex", borderRadius: "200px 200px", border: "10px ridge grey"}} alt="Foto de perfil"/>
                 </div>
-                <div style={{height: "20vh", width: "100vh", boxSizing: "border-box" ,display: "flex", flexDirection: "column", justifyContent: "space-evenly"}}>
-                    <h1 style={{color: "white"}}>Hey, I'm Fernando Amorim!</h1>
-                    <p style={{color: "white"}}>I'm a Web Developer. I focus on creating digital experiences that are both functional and visually engaging..</p>
-                    <SocialLinks/>
+                <div style={{fontFamily: "Courier New", height: "20vh", width: "100vh", boxSizing: "border-box" ,display: "flex",  flexDirection: "column", justifyContent: "space-evenly"}}>
+                    <h1 style={{color: "white",fontWeight: "bold", fontSize: "35px"}}>
+                        Hey, I'm <span style={{color: "#7CF24B"}}>Fernando Amorim!</span>
+                         </h1>
+                    <p style={{color: "white", fontSize: "20px"}}>
+                        I'm a <span style={{color: "#7CF24B"}}> Web Developer</span>. I focus on creating digital experiences that are both functional and visually engaging.
+                        </p>
+                    <div style={{ display: "flex", flexDirection: "row", gap: "20px", boxSizing: "border-box", flexWrap: "wrap"}}>
+                        <SocialLinks/>
+                        <Link href="https://www.meucurriculoperfeito.com.br/feedback/session/b8fed46f-81e2-4a76-9ff1-e9c2130d5e97" target="_blank" style={{padding: "20px", display: "flex", justifyContent: "flex-start"}}>
+                            <Button className={styles.download} style={{fontSize: "17px", padding: "10px 30px"}}> Download CV </Button>
+                        </Link>
+                    </div>
                 </div>    
+                
             </div>
 
         </main>
